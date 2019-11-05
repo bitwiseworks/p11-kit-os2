c8d009a9e90a0862b3b3b56f74ce1172f3fd80de
SilvanScherrer <silvan.scherrer@aroa.ch>
2019-11-05 17:53:59 +0100

    disable getuid(), as we are always 0 and that's ok

--- p11-kit/conf.c
+++ p11-kit/conf.c
@@ -232,7 +232,7 @@ _p11_conf_load_globals (const char *system_conf, const char *user_conf,
 		if (getauxval (AT_SECURE)) {
 			p11_debug ("skipping user config in setuid or setgid program");
 			mode = CONF_USER_NONE;
-#ifdef OS_UNIX
+#if defined(OS_UNIX) && !defined(__OS2__)
 		} else if (getuid () == 0) {
 			p11_debug ("skipping user config in program running as root");
 			mode = CONF_USER_NONE;
